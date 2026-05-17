2b98a615007e05b5a13f5517b9cc205dc2e702c5
wintermutant <ddeemer@purdue.edu>
2026-05-16 20:35:51 -0400

    updated old engine logic that is deprecated - saved for testing purpose

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
+'''
+The scope of this is to have a database as a service. We don't want super complex logic.
+Basically we just want to make the models available and allow data to be read in via:
+- stream io
+- pathlike object
+- dictionary / in memory
+
+As far as connecting to the engine and creating the tables (engine.create_all()), we want
+this to be as simple as possible. For example, another python package might do:
+from gmodels.biomodels import Sequence, Bioentry
+from gmodels.app import initialize
+engine = create_engine(...)
+SQLModel.metadata.create_all(engine)
+>>> conversely, do:
+engine = initialize(engine_string="...")  # returns an engine object
+mydata = Sequence(**raw_seq_dictionary)
+with Session(engine) as session:
+    session.add(mydata).commit()
+
+This allows them to define their own engine or use ours boilerplate stuff. We may have a quick add:
+mydata.add_and_commit()
+>>> Above, this will open a session for them, add it, and commit, but not recommend
+
+'''
+import os
 import time
+import sys
 
-from sqlmodel import Field, Session, SQLModel, create_engine
+from dotenv import load_dotenv
+from sqlmodel import SQLModel, create_engine
 
-from data import SEQUENCES
-import sql_models as sqlm
+load_dotenv()
 
-DB_CONNECTION = "postgresql+psycopg2://dane:deemer@localhost:5432/genomics"
+BIOMODELS_DB_CONNECTION = os.getenv("BIOMODELS_DB_CONNECTION")
+if not BIOMODELS_DB_CONNECTION:
+    sys.exit('Need to set the BIOMODELS_DB_CONNECTION env variable in .env')
+
+print(f'BIOMODELS...: {BIOMODELS_DB_CONNECTION}')
+
 
 engine = create_engine(
-    'postgresql+psycopg2://dane:deemer@localhost:5432/genomics',
+    BIOMODELS_DB_CONNECTION,
     connect_args={"application_name": "debug_connection"},
     echo=True
     )
@@ -23,21 +54,9 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
-def create_sequence_models(raw_sequence_data: list[dict]):
-    my_models: list[sqlm.Sequence] = []
-    for data in raw_sequence_data:
-        my_models.append(sqlm.Sequence(**data))
-    return my_models
-
-def updata_sequence_table(my_models: list[sqlm.Sequence]):
-    with Session(engine) as session:
-        for model in my_models:
-            session.add(model)
-            session.commit()
-
 
 if __name__ == "__main__":
     create_db_and_tables()
-    modeled_data = create_sequence_models(SEQUENCES)
-    print(f'My models:\n{modeled_data}')
-    updata_sequence_table(modeled_data)
+    # modeled_data = create_sequence_models(SEQUENCES)
+    # print(f'My models:\n{modeled_data}')
+    # updata_sequence_table(modeled_data)
